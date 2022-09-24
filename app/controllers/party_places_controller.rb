748daa2f8413c04433d4be604b2d7607b4d6423c
James Almeida <james.almeida.ti@gmail.com>
2022-09-24 01:34:36 -0300

    Add party_place relationships on find and list

--- app/controllers/party_places_controller.rb
+++ app/controllers/party_places_controller.rb
@@ -2,25 +2,22 @@ class PartyPlacesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    party_places = nil
-
     if params.has_key?(:party_place_name)
-      name = params[:party_place_name]
-      party_places = PartyPlace.where(name: name)
+      @party_places = PartyPlace.where(name: params[:party_place_name])
 
-      if party_places.empty?
+      if @party_places.empty?
         render json: { errors: 'Estabelecimento não encontrado' }, status: :not_found
       else
-        render json: party_places, status: :ok
+        render status: :ok, template: "party_places"
       end
     else
-      party_places = PartyPlace.all
-      paginate json: party_places
+      @party_places = paginate PartyPlace.all
+      render status: :ok, template: "party_places"
     end
   end
 
   def show
-    party_place = PartyPlace.find(params[:id])
-    render json: party_place, status: :ok
+    @party_places = PartyPlace.find(params[:id])
+    render status: :ok, template: "party_places"
   end
 end
