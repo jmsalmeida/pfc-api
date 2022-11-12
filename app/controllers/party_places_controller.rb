9c05a04c1ceaae29b3ed9fb12e23f3fc422138a2
James Almeida <james.almeida.ti@gmail.com>
2022-11-12 17:29:21 -0300

    Refactor party_places controller index action
    - Search party place by name using partial strings
    - Allow filter party places by its party feature tags

--- app/controllers/party_places_controller.rb
+++ app/controllers/party_places_controller.rb
@@ -2,13 +2,16 @@ class PartyPlacesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    if params.has_key?(:party_place_name)
-      @party_places = PartyPlace.where(name: params[:party_place_name])
+    filter_params = [:party_features, :party_place_name]
 
-      if @party_places.empty?
-        render json: { errors: 'Estabelecimento não encontrado' }, status: :not_found
-      else
+    if filter_params.any? {|key| params.has_key? key}
+      filter_by_party_features
+      search_party_place_by_name
+
+      unless @party_places.empty?
         render status: :ok, template: "party_places"
+      else
+        render json: { errors: [I18n.t('errors.controllers.party_places.not_found')] }, status: :not_found
       end
     else
       @party_places = paginate PartyPlace.all
@@ -20,4 +23,25 @@ class PartyPlacesController < ApplicationController
     @party_places = PartyPlace.find(params[:id])
     render status: :ok, template: "party_places"
   end
-end
+
+  private
+
+  def filter_by_party_features
+    if params.has_key?(:party_features)
+      @party_place_ids = PartyFeature.tagged_with(params[:party_features], :any => true).pluck(:party_place_id)
+      @party_places = PartyPlace.find(@party_place_ids)
+    end
+  end
+
+  def search_party_place_by_name
+    if params.has_key?(:party_place_name)
+      @search = params[:party_place_name]
+
+      if @party_places.blank?
+        @party_places = PartyPlace.where("name LIKE ?", "%#{@search}%")
+      else
+        @party_places = PartyPlace.where(id: @party_place_ids).where("name LIKE ?", "%#{@search}%")
+      end
+    end
+  end
+  end
