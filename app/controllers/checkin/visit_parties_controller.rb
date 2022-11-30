830b8d02b4d3dbfbfe8cdd545bfe226900292e76
James Almeida <james.almeida.ti@gmail.com>
2022-11-30 01:07:08 -0300

    Remove checkin time verification for empty checkins

--- app/controllers/checkin/visit_parties_controller.rb
+++ app/controllers/checkin/visit_parties_controller.rb
@@ -7,7 +7,7 @@ class Checkin::VisitPartiesController < ApplicationController
     @partyer = Partyer.find_by(user_id: @current_user.id)
     @party_place = PartyPlace.find params[:party_place_id]
 
-    if allowed_to_checkin?
+    if @party_place.checkins.empty? || allowed_to_checkin?
       @checkin = @party_place.checkins.create partyer_id: @partyer.id
       render json: @checkin, status: :created
     else
@@ -17,9 +17,10 @@ class Checkin::VisitPartiesController < ApplicationController
 
   private
   def allowed_to_checkin?
-    last_checkin = @party_place.checkins.where(partyer_id: @partyer.id).last
-    last_checkin_time = last_checkin.created_at.in_time_zone BR_TIME_ZONE
+    current_user_checkins = @party_place.checkins.where(partyer_id: @partyer.id)
+    return true if current_user_checkins.empty?
 
+    last_checkin_time = current_user_checkins.last.created_at.in_time_zone BR_TIME_ZONE
     time_diff = (Time.current.in_time_zone(BR_TIME_ZONE) - last_checkin_time)
     diff_considering_minutes = (time_diff / 1.minute).round
 
